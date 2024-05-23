2b746e2cddd5bca5ec971abce8eb7359a75d4334
unknown <suhrguy@gmail.com>
2024-05-23 12:00:13 -0400

    basic addition development

--- pycalculator.py
+++ pycalculator.py
@@ -4,10 +4,23 @@ os.system("cls")
 
 def display_title():
     title = "Calculator"
-    padded_text = title.center(118)
+    padded_text = title.center(150)
     print(padded_text)
 
+def question_loop():
+    print("Please type a number you would like to add.")
+    userResponse1 = input()
+    print("Thank you. Now type in a second number")
+    userResponse2 = input()
+    num1 = int(userResponse1)
+    num2 = int(userResponse2)
+    total = num1 + num2
+    print("Your total is:",total)
+
 display_title()
+print("\n")
+print("\n")
+question_loop()
 input()
 
 
