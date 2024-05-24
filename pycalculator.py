6aa26f1f000ad6af8e9073bfea0600ed3ab8ce32
Taylor Sawyer <suhrguy@gmail.com>
2024-05-24 06:52:45 -0400

    input error handling test

--- pycalculator.py
+++ pycalculator.py
@@ -8,15 +8,18 @@ def display_title():
     print(padded_text)
 
 def question_loop():
-    print("Please type a number you would like to add.")
-    userResponse1 = input()
-    print("Thank you. Now type in a second number")
-    userResponse2 = input()
-    num1 = int(userResponse1)
-    num2 = int(userResponse2)
-    total = num1 + num2
-    print(f"{num1} + {num2} = {total}")
-    return total
+    try:
+        print("Please type a number you would like to add.")
+        userResponse1 = input()
+        print("Thank you. Now type in a second number")
+        userResponse2 = input()
+        num1 = int(userResponse1)
+        num2 = int(userResponse2)
+        total = num1 + num2
+        print(f"{num1} + {num2} = {total}")
+        return total
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
 display_title()
 print("\n")
